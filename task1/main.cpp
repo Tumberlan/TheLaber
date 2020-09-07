3559b1d88ced317524e35077cc371e89c9e8af7f
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2020-09-07 21:06:34 +0700

    07.09.2020

---
+++ task1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int get_rnk_block(char rnk, int number){
+    for(int i = 4-number; i > 0; i--){
+        rnk = rnk >> 2;
+    }
+    return rnk & 0x03;
+}
+
+int main() {
+    char rnk = 0x3B;
+    int a;
+    std:: cin >> a;
+    std::cout << get_rnk_block(rnk, a);
+    return 0;
+}
