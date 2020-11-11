f95f6cdaedcbf5ecf1a82713a4bfc6a7addfe408
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2020-11-11 17:31:20 +0700

    11.11.2020 17:31

---
+++ lab1/main.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+#include "rna.h"
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    RNA a;
+    RNA b;
+
+    for (int i = 0; i < 49; i++){
+        a[i] = T;
+    }
+    for (int i = 0; i < 49; i++){
+        std:: cout << a[i];
+    }
+    std:: cout << '\n';
+    for (int i = 0; i < 49; i++){
+        b[i] = G;
+    }
+    for (int i = 0; i < 49; i++){
+        std:: cout << b[i];
+    }
+    std:: cout << '\n';
+    RNA sum = a+b;
+    for (int i = 0; i < 50; i++){
+        std:: cout << sum[i];
+    }
+
+    std:: cout << '\n' << "123";
+    return 0;
+
+}
+
+
+
+
+/*
+int main(int argc, char *argv[]){
+
+
+
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}*/
